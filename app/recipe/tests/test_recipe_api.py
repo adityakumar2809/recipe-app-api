4543a5c052ca526e9e5d66747282356b6550b8e4
Aditya Kumar Singh <adityakumar2809@gmail.com>
2020-12-16 17:14:32 +0530

    Add feature to return Recipe Detail View

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -13,6 +13,21 @@ from recipe import serializers
 RECIPES_URL = reverse('recipe:recipe-list')
 
 
+def detail_url(recipe_id):
+    """Return recipe detail url"""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
+
+
+def sample_tag(user, name='Main course'):
+    """Create and return a sample tag"""
+    return models.Tag.objects.create(user=user, name=name)
+
+
+def sample_ingredient(user, name='Water'):
+    """Create and return a sample ingredient"""
+    return models.Ingredient.objects.create(user=user, name=name)
+
+
 def sample_recipe(user, **params):
     """Create and return a sample recipe"""
     defaults = {
@@ -80,4 +95,16 @@ class PrivateRecipeApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_view_recipe_detail(self):
+        """Test viewing a recipe detail"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = serializers.RecipeDetailSerializer(recipe)
         self.assertEqual(res.data, serializer.data)
